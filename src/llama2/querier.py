2ddb42b336035ceb4d454aec92a717150400fd08
Flavio <flavio.petruzzellis@gmail.com>
2024-01-24 11:21:19 +0100

    Adapt querier to metamath

--- src/llama2/querier.py
+++ src/llama2/querier.py
@@ -42,7 +42,7 @@ class ModelQuerierOnTask:
 			if (self.prompt_type == 'zero_shot_cot'):
 				output = self.hfi.query_model_zero_shot_cot([prompt])[0]
 				zero_shot_cot_first_out = self.hfi.zero_cot_first_outputs[0]
-			elif 'mammoth' in self.model_name:
+			elif ('mammoth' in self.model_name) or ('metamath' in self.model_name):
 				output = self.hfi.query_model([prompt], None, structured_prompt=False)[0]
 				zero_shot_cot_first_out = None
 			else:
